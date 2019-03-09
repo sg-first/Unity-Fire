757cafa5add32790005c4274d0674ed28be3913c
3A <524019351@qq.com>
2019-03-09 15:45:30 +0800

    lookAt

--- unityFire.cs
+++ unityFire.cs
@@ -7,46 +7,59 @@ class unityFire : MonoBehaviour
 {
 	public static void moveByItem(GameObject item,Vector3 pos)
 	{
-			item.transform.Translate(pos);
+        //也可以写成transform.Translate(Vector3 * Speed);
+        item.transform.Translate(pos);
 	}
+
+    public static void moveTimeItem(GameObject item, float time, float speed, Vector3 angle = default(Vector3))
+    {
+        item.GetComponent<move>().moveTime(time, speed, angle);
+    }
 
 	public static GameObject addItem(string objname,Vector3 pos,Vector3 angle=default(Vector3))
 	{
-			GameObject itemp = Resources.Load<GameObject> (objname);
-			Quaternion qangle;
-			if (angle == default(Vector3))
-					qangle = itemp.transform.rotation;
-			else
-					qangle = calu.Vector3ToQuaternion(angle);
-			return Instantiate (itemp, pos, qangle);
+		GameObject itemp = Resources.Load<GameObject> (objname);
+		Quaternion qangle;
+		if (angle == default(Vector3))
+				qangle = itemp.transform.rotation;
+		else
+				qangle = calu.Vector3ToQuaternion(angle);
+		return Instantiate (itemp, pos, qangle);
 	}
 
 	public static void deleteItem(GameObject item)
 	{
-			Destroy (item);
+		Destroy (item);
 	}
 
 	public static Vector3 getItemPos(GameObject item)
 	{
-			return item.transform.position;
+		return item.transform.position;
 	}
 
-	public static Vector3 getItemAngle(GameObject item)
+    public static void setItemPos(GameObject item, Vector3 pos)
+    {
+        item.transform.position = pos;
+    }
+
+    public static Vector3 getItemAngle(GameObject item) //返回方向向量
 	{
-			Quaternion q=item.transform.rotation;
-			return calu.QuaternionToVector3(q);
-	}
+	    Quaternion q=item.transform.rotation;
+        Vector3 result = calu.QuaternionToVector3(q);
+        result.Normalize();
+        return result; 
+    }
 
 	public static void rotateByItem(GameObject item,Vector3 angle) //直接传旋转角
 	{
-			item.transform.Rotate(angle,Space.Self);
+		item.transform.Rotate(angle,Space.Self);
 	}
 
 	public static void addItemForce(GameObject item,float power,Vector3 angle) //一般angle传up right forward或者基于此的变换
 	{
-			Rigidbody rb = item.GetComponent<Rigidbody> ();
-			Vector3 pos = item.transform.TransformDirection(angle);
-			rb.AddForce(pos * power);
+		Rigidbody rb = item.GetComponent<Rigidbody> ();
+		Vector3 pos = item.transform.TransformDirection(angle);
+		rb.AddForce(pos * power);
 	}
 
 	public static void setItemVelocity(GameObject item,float v,Vector3 angle)
@@ -63,17 +76,17 @@ class unityFire : MonoBehaviour
 
 	public static GameObject findItem(string itemName)
 	{
-			return GameObject.Find(itemName);
+		return GameObject.Find(itemName);
 	}
 
 	public static void exit()
 	{
-			Application.Quit();
+		Application.Quit();
 	}
 
 	public static void changeScene(string sceneName)
 	{
-			SceneManager.LoadScene(sceneName);
+		SceneManager.LoadScene(sceneName);
 	}
 
 	public static void setCameraDepth(string cameraName,int depth)
@@ -83,16 +96,23 @@ class unityFire : MonoBehaviour
 		c.depth = depth;
 	}
 
-        public static Vector3 getMousePos()
-        {
-            return Input.mousePosition;
-        }
+    public static Vector3 getMousePos()
+    {
+        return Input.mousePosition;
+    }
 
-        public static Vector3 toWorldPos(Vector3 mousePos)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(mousePos);
-            RaycastHit hit;
-            Physics.Raycast(ray, out hit);
-            return hit.point;
-        }
+    public static Vector3 toWorldPos(Vector3 mousePos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        return hit.point;
+    }
+
+    public static Vector3 lookAt(GameObject itemA, GameObject itemB) //B撞A，B是参数A是this
+    {
+        Vector3 result = getItemPos(itemA) - getItemPos(itemB);
+        result.Normalize();
+        return result;
+    }
 }
