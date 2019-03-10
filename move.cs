e7bdef49f6d4126270be2a3d0ffd173f2da733fc
3A <524019351@qq.com>
2019-03-10 15:46:08 +0800

    moveTimeItem

---
+++ move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    float nowTime;
+    float targetTime;
+    float speed;
+    Vector3 angle;
+    bool isMove = false;
+
+    public void moveTime(float time,float speed,Vector3 angle = default(Vector3))
+    {
+        if (angle == default(Vector3))
+            angle = unityFire.getItemAngle(this.gameObject);
+        this.angle = angle;
+        this.speed = speed;
+        this.targetTime = time;
+        this.nowTime = 0;
+        this.isMove = true;
+    }
+
+    void FixedUpdate()
+    {
+        if(isMove)
+        {
+            nowTime += Time.deltaTime;
+            Debug.Log(nowTime);
+            if (nowTime < targetTime)
+            {
+                transform.Translate(angle * speed * Time.deltaTime);
+            }
+            else
+            {
+                isMove = false;
+            }
+        }
+    }
+}
