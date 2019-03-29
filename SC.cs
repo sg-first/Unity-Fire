437405e79ed7f770e525dd1bc011fff6c6395ddc
3A <524019351@qq.com>
2019-03-29 12:51:06 +0800

    SC支持旋转，但还没测试

--- SC.cs
+++ SC.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class SC : MonoBehaviour
 {
-    float nowTime;
-    float targetTime;
+    //move
+    float moveNowTime;
+    float moveTargetTime;
     float speed;
     Vector3 angle;
     bool isMove = false;
+    //rotate
+    float rotateNowTime;
+    float rotateTargetTime;
+    Vector3 rotateSpeed;
+    bool isRotate = false;
 
     public void moveTime(float time,float speed,Vector3 angle = default(Vector3))
     {
@@ -16,23 +22,57 @@ public class SC : MonoBehaviour
             angle = unityFire.getItemAngle(this.gameObject);
         this.angle = angle;
         this.speed = speed;
-        this.targetTime = time;
-        this.nowTime = 0;
+        this.moveTargetTime = time;
+        this.moveNowTime = 0;
         this.isMove = true;
+    }
+
+    public void moveTimeTarget(float time,Vector3 target)
+    {
+        Vector3 pos1 = unityFire.getItemPos(this.gameObject);
+        Vector3 angle = target - pos1;
+        float length = angle.magnitude;
+        angle.Normalize();
+        this.moveTime(time, length / time, angle);
+    }
+
+    public void rotateTime(float time,Vector3 rotateSpeed)
+    {
+        this.rotateSpeed = rotateSpeed;
+        this.rotateNowTime = 0;
+        this.rotateTargetTime = time;
+        this.isRotate = true;
+    }
+
+    public void rotateTimeTarget(float time,Vector3 rotateTarget)
+    {
+        this.rotateTime(time, rotateTarget / time);
     }
 
     void FixedUpdate()
     {
         if(isMove)
         {
-            nowTime += Time.deltaTime;
-            if (nowTime < targetTime)
+            moveNowTime += Time.deltaTime;
+            if (moveNowTime < moveTargetTime)
             {
                 transform.Translate(angle * speed * Time.deltaTime);
             }
             else
             {
                 isMove = false;
+            }
+        }
+        if(isRotate)
+        {
+            rotateNowTime += Time.deltaTime;
+            if(rotateNowTime<rotateTargetTime)
+            {
+                transform.Rotate(rotateSpeed);
+            }
+            else
+            {
+                isRotate = false;
             }
         }
     }
