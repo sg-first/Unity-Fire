5121fcf6fdb0b51a5b1f2ff6ffcc1089aad1c78f
Mingkai Zhou <524019351@qq.com>
2019-04-13 00:08:12 +0800

    直接用现有的外积计算外加归一化

--- calu.cs
+++ calu.cs
@@ -29,8 +29,8 @@ class calu : MonoBehaviour
 
     public static Vector3 normalVector(Vector3 v1, Vector3 v2)
     {
-        return new Vector3(v1.y * v2.z - v1.z * v2.y,
-                            v1.x * v2.z - v1.z * v2.x,
-                            v1.x * v2.y - v1.y * v2.x);
+        Vector3 result = Vector3.Cross(v1, v2);
+        result.Normalize();
+        return result;
     }
 }
